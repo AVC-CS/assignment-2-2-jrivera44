8e47d86556e3c99aaebf6fe72f4fe8d56ea5a193
jrivera44 <149451158+jrivera44@users.noreply.github.com>
2023-11-14 20:22:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     """
     ##################################################
-    # celcius = int(input())
+    celcius = int(input())
     fahrenheit = (celcius * 9 / 5) + 32
     print(f'{fahrenheit:.2f}')
     ##################################################
